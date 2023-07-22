32e213e2878f1f88ac36e62c03cedb040f8e2f66
engadnanhossain <engr.adnanhossain@gmail.com>
2023-07-22 19:08:52 +0600

    add featured category

---
+++ src/components/home/featureCategory/FeatureCategory.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaTv } from "react-icons/fa";
+
+
+const FeatureCategory = () => {
+    return (
+        <div className='mb-5'>
+            <div className='text-center'>
+                <h4>Featured Category</h4>
+                <p>Get Your Desired Product from Featured Category!</p>
+            </div>
+
+            {/* category component */}
+            <div className='row row-cols-3 row-cols-md-3 row-cols-lg-6 m-0'>
+                {
+                    Array.from({ length: 12 }).map((fcat, index) =>
+                        <div key={index} className='col mb-3'>
+                            <div className='rounded bg-light p-4 shadow-sm d-flex flex-column justify-content-center align-items-center' style={{ cursor: 'pointer' }}>
+                                <FaTv className='fs-1'></FaTv>
+                                <p>Tv</p>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+
+
+        </div>
+    );
+};
+
+export default FeatureCategory;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Index from '../../components/home/Banner/Index';
 import Marquee from '../../components/home/Marquee/Marquee';
+import FeatureCategory from '../../components/home/featureCategory/FeatureCategory';
 
 const Home = () => {
     return (
         <div className='container'>
             <Index/>
             <Marquee/>
+            <FeatureCategory/>
         </div>
     );
 };
